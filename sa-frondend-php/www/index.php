6e94c5ae3a54a012c91404a67ce61f393b7e63ca
djkormo <kpudlowski@plus.pl>
2019-07-15 14:35:20 +0200

    correcting index.php

--- sa-frondend-php/www/index.php
+++ sa-frondend-php/www/index.php
@@ -10,12 +10,23 @@
 <body>
   <h1>My PHP Website</h1>
   <p>Here is some static content.</p>
-  <p><?php echo "Here is some dynamic content"; ?></p>
   
   
-  <?php
-  phpinfo();
-  ?>
+   <form action="index.php">
+            <input type="text" name="sentiment" />
+            <input type="submit" name="insert" value="insert" onclick="insert()" />
+            <input type="submit" name="select" value="select" onclick="select()" />
+        </form>
+
+        <?php
+            function select(){
+               echo "The select function is called.";
+            }
+            function insert(){
+               echo "The insert function is called.";
+            }
+        ?>
+  
   
   <?php
 //API URL
