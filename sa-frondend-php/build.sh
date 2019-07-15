945f2732008b1a84457280e7d384c531b71a64bc
djkormo <kpudlowski@plus.pl>
2019-07-15 14:05:29 +0200

    adding sa-frondend-php/Dockerfile

---
+++ sa-frondend-php/build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DOCKER_REGISTRY=docker.io
+DOCKER_PROJECT_ID=djkormo
+SERVICE_NAME=sentiment-analysis-frontend-php
+DOCKER_IMAGE_NAME=$DOCKER_PROJECT_ID/$SERVICE_NAME
+DOCKER_IMAGE_REPO_NAME=$DOCKER_REGISTRY/$DOCKER_IMAGE_NAME
+
+echo "DOCKER_REGISTRY: $DOCKER_REGISTRY"
+echo "DOCKER_PROJECT_ID: $DOCKER_PROJECT_ID"
+echo "SERVICE_NAME: $SERVICE_NAME"
+echo "DOCKER_IMAGE_NAME: $DOCKER_IMAGE_NAME"
+echo "DOCKER_IMAGE_REPO_NAME: $DOCKER_IMAGE_REPO_NAME"
+
+#  build 
+
+docker build -t $SERVICE_NAME . -f Dockerfile
+docker build -t $SERVICE_NAME:green . -f Dockerfile-green
+# tag
+
+docker tag $SERVICE_NAME $DOCKER_IMAGE_NAME 
+docker tag $SERVICE_NAME:green $DOCKER_IMAGE_NAME:green 
+
+#push
+
+docker push  $DOCKER_IMAGE_NAME
+docker push  $DOCKER_IMAGE_NAME:green
+
+
+
---
+++ sa-frondend-php/www/index.php
@@ -0,0 +1,47 @@
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>My PHP Website</title>
+</head>
+
+<body>
+  <h1>My PHP Website</h1>
+  <p>Here is some static content.</p>
+  <p><?php echo "Here is some dynamic content"; ?></p>
+  
+  <?php
+//API URL
+$url = 'http://www.example.com/api';
+
+//create a new cURL resource
+$ch = curl_init($url);
+
+//setup request to send json via POST
+$data = array(
+    'sentence' => 'sentence',
+    'content' => 'I like my mother'
+);
+$payload = json_encode(array("user" => $data));
+
+//attach encoded JSON string to the POST fields
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+//set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+//return response instead of outputting
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//execute the POST request
+$result = curl_exec($ch);
+
+//close cURL resource
+curl_close($ch);
+
+?>
+</body>
+</html>
+
