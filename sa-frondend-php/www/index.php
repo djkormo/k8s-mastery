acb9b6a4938532136fccdac03fddfd8c9ad2f659
djkormo <kpudlowski@plus.pl>
2019-07-15 14:50:50 +0200

    correcting sa-frondend-php/www/index.php

--- sa-frondend-php/www/index.php
+++ sa-frondend-php/www/index.php
@@ -15,21 +15,34 @@
   <p>Here is some static content.</p>
   
   
-   <form action="index.php">
-            <input type="text" name="sentiment" />
-            <input type="submit" name="insert" value="insert" onclick="insert()" />
-            <input type="submit" name="select" value="select" onclick="select()" />
-        </form>
-
-        <?php
-            function select(){
-               echo "The select function is called.";
-            }
-            function insert(){
-               echo "The insert function is called.";
-            }
-        ?>
+<form action="" method="POST">
+<label>Enter Sentence::</label><br />
+<input type="text" name="sentiment" placeholder="Enter  Sentence" required/>
+<br /><br />
+<button type="submit" name="submit">Submit</button>
+</form>
   
+ 
+<?php
+if (isset($_POST['sentiment']) && $_POST['sentiment']!="") {
+	$sentiment = $_POST['sentiment'];
+	$url = "http://localhost:8000/sentiment/".$sentiment;
+	
+	$client = curl_init($url);
+	curl_setopt($client,CURLOPT_RETURNTRANSFER,true);
+	$response = curl_exec($client);
+	
+	$result = json_decode($response);
+	
+	echo "<table>";
+	echo "<tr><td>Order ID:</td><td>$result->sentiment</td></tr>";
+	echo "<tr><td>Amount:</td><td>$result->amount</td></tr>";
+	echo "<tr><td>Response Code:</td><td>$result->response_code</td></tr>";
+	echo "<tr><td>Response Desc:</td><td>$result->response_desc</td></tr>";
+	echo "</table>";
+}
+    ?>
+ 
   
   <?php
 //API URL
