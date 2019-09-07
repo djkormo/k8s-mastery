88c0695df6119a434af69f87a1be51bffebe55ae
Twoje imię i nazwsko <djkormo@gmail.com>
2019-09-08 01:11:40 +0200

    adding test4.php

---
+++ sa-frontend-php/www/test4.php
@@ -0,0 +1,31 @@
+<?php
+
+$url = "http://ip172-18-0-8-blq190ad7o0g00edt8d0-8080.direct.labs.play-with-docker.com/sentiment/";    
+$data=array('sentiment' => 'I like yogobella');
+$content = json_encode($data);
+
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_HEADER, false);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($curl, CURLOPT_HTTPHEADER,
+        array("Content-type: application/json"));
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
+
+$json_response = curl_exec($curl);
+
+$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+if ( $status != 201 ) {
+    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+}
+
+curl_close($curl);
+
+$response = json_decode($json_response, true);
+echo "<pre>";
+print (CURLINFO_HTTP_CODE);
+print_r($response);
+echo "</pre>";
+
+?>
