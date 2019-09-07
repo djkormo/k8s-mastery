b8f4d8c0c9039dd5b8458746911de4ae88c1958b
Twoje imię i nazwsko <djkormo@gmail.com>
2019-09-07 23:51:39 +0200

    correcting test.php

--- sa-frontend-php/www/test.php
+++ sa-frontend-php/www/test.php
@@ -1,37 +1,37 @@
 <?php
-
+###
 
 # force displaying errors
 error_reporting(E_ALL);
 
 ini_set("display_errors", 1);
 
-//API Url
-$url = '"http://localhost:8000/sentiment/';
- 
-//Initiate cURL.
-$ch = curl_init($url);
- 
-//The JSON data.
-$jsonData = array(
-    'sentence' => 'I like playing footbal'
-);
- 
-//Encode the array into JSON.
-$jsonDataEncoded = json_encode($jsonData);
- 
-//Tell cURL that we want to send a POST request.
-curl_setopt($ch, CURLOPT_POST, 1);
- 
-//Attach our encoded JSON string to the POST fields.
-curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonDataEncoded);
- 
-//Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json')); 
- 
-//Execute the request
 
+$url = 'http://ip172-18-0-8-blq190ad7o0g00edt8d0-8080.direct.labs.play-with-docker.com/sentiment/';
+
+//create a new cURL resource
+$ch = curl_init($url);
+
+//setup request to send json via POST
+$data = array(
+    'username' => 'codexworld',
+    'password' => '123456'
+);
+$payload = json_encode(array("user" => $data));
+
+//attach encoded JSON string to the POST fields
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+
+//set the content type to application/json
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+//return response instead of outputting
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+//execute the POST request
 $result = curl_exec($ch);
-print_r($result);
+
+//close cURL resource
+curl_close($ch);
 
 ?>
