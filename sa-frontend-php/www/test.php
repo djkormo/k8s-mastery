8606076ce527df5d679d6bf963fa5d2e7c54401e
Twoje imię i nazwsko <djkormo@gmail.com>
2019-09-07 23:56:02 +0200

    correcting test.php

--- sa-frontend-php/www/test.php
+++ sa-frontend-php/www/test.php
@@ -7,6 +7,27 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 
+$b = array(1, 1, 2, 3, 5, 8);
+
+$arr = get_defined_vars();
+
+// print $b
+print_r($arr["b"]);
+
+/* print path to the PHP interpreter (if used as a CGI)
+ * e.g. /usr/local/bin/php */
+echo $arr["_"];
+
+// print the command-line parameters if any
+print_r($arr["argv"]);
+
+// print all the server vars
+print_r($arr["_SERVER"]);
+
+// print all the available keys for the arrays of variables
+print_r(array_keys(get_defined_vars()));
+
+
 $url = 'http://ip172-18-0-8-blq190ad7o0g00edt8d0-8080.direct.labs.play-with-docker.com/sentiment/';
 
 //create a new cURL resource
@@ -33,5 +54,8 @@ $result = curl_exec($ch);
 
 //close cURL resource
 curl_close($ch);
+
+
+
 
 ?>
