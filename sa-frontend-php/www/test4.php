2477de133a5808f6ffd265194bcc2621ccf977d8
Twoje imię i nazwsko <djkormo@gmail.com>
2019-09-08 01:45:31 +0200

    correcting test4.php

--- sa-frontend-php/www/test4.php
+++ sa-frontend-php/www/test4.php
@@ -17,7 +17,7 @@ $json_response = curl_exec($curl);
 
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
-if ( $status != 201 ) {
+if ( $status > 200 ) {
     die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
 }
 
