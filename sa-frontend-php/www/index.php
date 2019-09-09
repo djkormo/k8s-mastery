d955c9e3a8b197aa3598326c6d751c0bd212133d
djkormo <kpudlowski@plus.pl>
2019-09-09 09:03:54 +0200

    correcting index.php

--- sa-frontend-php/www/index.php
+++ sa-frontend-php/www/index.php
@@ -55,15 +55,17 @@ $json_response = curl_exec($curl);
 
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
+echo "<pre>";
 
+print_r($data);
 if ( $status > 200 ) {
-    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+    print("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
 }
 
 curl_close($curl);
 
 $response = json_decode($json_response, true);
-echo "<pre>";
+
 
 print (CURLINFO_HTTP_CODE);
 
--- sa-frontend-php/www/test4.php
+++ sa-frontend-php/www/test4.php
@@ -18,17 +18,17 @@ curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
 $json_response = curl_exec($curl);
 
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
+echo "<pre>";
 print($status);
 
 if ( $status > 200 ) {
-    die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+    print("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
 }
 
 curl_close($curl);
 
 $response = json_decode($json_response, true);
-echo "<pre>";
+
 print_r($data);
 print (CURLINFO_HTTP_CODE);
 print($status);
