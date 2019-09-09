7e3ffd183b3047163fe321b6d3a860f8d9b44533
djkormo <kpudlowski@plus.pl>
2019-09-09 09:17:11 +0200

    correcting index.php

--- sa-frontend-php/www/index.php
+++ sa-frontend-php/www/index.php
@@ -35,7 +35,7 @@ $url = getenv('SA_WEBAPP_API_URL');
 
 print("Your endpoint->: ".$url); 
  
-if((isset($_POST['sentence']) && $_POST['sentence']!="")
+#if((isset($_POST['sentence']) && $_POST['sentence']!="")
 {
    
 $data=array('sentence' => $_POST['sentence']
@@ -71,7 +71,7 @@ print (CURLINFO_HTTP_CODE);
 print_r($response);
 echo "</pre>";
 
-} // of if 
+//} // of if 
 
 echo "<pre>";
     print_r($_POST);
