accfffa3b8b1ab73911a0b57a2beff2c164ee5e0
djkormo <kpudlowski@plus.pl>
2019-09-09 10:52:06 +0200

    correcting index.php

--- sa-frontend-php/www/index.php
+++ sa-frontend-php/www/index.php
@@ -59,9 +59,9 @@ $json_response = curl_exec($curl);
 $status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 
 echo "<pre>";
-print ("Input: \n");
+print ("\nInput: \n");
 print($sentence);
-print ("Input 2: \n");
+print ("\nInput 2: \n");
 print_r($data);
 if ( $status > 200 ) {
     print("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
@@ -73,7 +73,7 @@ $response = json_decode($json_response, true);
 
 print ("CURLINFO_HTTP_CODE: \n");
 print (CURLINFO_HTTP_CODE);
-print ("OUTPUT: \n");
+print ("\nOUTPUT: \n");
 print_r($response);
 echo "</pre>";
 
